af4a899983469a5916e9e77ef6d2add4f8d7eb9e
vorvit <94832434+vorvit@users.noreply.github.com>
2021-12-29 14:42:00 +0300

    Добавьте файлы проекта.

---
+++ Models/CircleLength.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF19.Models
+{
+    static class CircleLength
+    {
+        public static double Add(double r) => Math.Round((2 * Math.PI * r), 2);
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF19.Models;
+
+namespace WPF19.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        
+        private double radius;
+        private double length;
+
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Length
+        {
+            get => length;
+            set
+            {
+                length = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Length = CircleLength.Add(Radius);
+        }
+
+        private bool CanAddCommandExecuted(object p)
+        {
+            return true;
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
